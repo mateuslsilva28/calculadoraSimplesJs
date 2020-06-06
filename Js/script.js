42bba6665258f7807daa6e2380ddcc3faa5cb621
Mateus <mateuslsilva28@gmail.com>
2020-06-05 22:33:01 -0300

    C2 - Ajuste de trocar sinal

--- Js/script.js
+++ Js/script.js
@@ -47,7 +47,6 @@ function colocarPonto(){
 }
 
 function mudarSinal(){
-    console.log(valores)
     if (valores.length == 0){
         var caracteres = nmrTela.innerHTML.split('')
         var nmrAtual = parseFloat(nmrTela.innerHTML);
@@ -66,26 +65,20 @@ function mudarSinal(){
         var nmrAtual = parseFloat(nmrTela.innerHTML);
         console.log(caracteres)
         let caracteres2Valor = caracteres[2].split('')
-        if(caracteres2Valor[0]!= " "){
-            alert('top')
-            caracteres = '-'
-            let novosCaracteres = []
-            novosCaracteres.concat(caracteres)
-            console.log(novosCaracteres)
+        if(caracteres2Valor[0] != "-"){
+            let novosCaracteres = '-'
+            for (let i in caracteres2Valor){
+                novosCaracteres += caracteres2Valor[i]
+            }
+            caracteres[2] = novosCaracteres
+            nmrTela.innerHTML = `${caracteres[0]} ${caracteres[1]} ${caracteres[2]}`
+        }
+        else {
+            let novosCaracteres = caracteres[2].split('')
+            novosCaracteres.shift()
+            nmrTela.innerHTML = `${caracteres[0]} ${caracteres[1]} ${novosCaracteres[0]}`
         }
     }
-    /*
-    var caracteres = nmrTela.innerHTML.split('')
-    var nmrAtual = parseFloat(nmrTela.innerHTML);
-    if(nmrAtual != 0){
-        if(caracteres[0] == "-"){        
-            caracteres.shift()
-            nmrTela.innerHTML = caracteres
-        } 
-        else {
-            nmrTela.innerHTML = `-${nmrAtual}`
-        }
-    }*/
 }
 
 function darResultado() {
